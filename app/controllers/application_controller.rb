4795a7ec54bef7bb01d4e07e0811f7268c01d910
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-03-01 17:48:58 +0300

    CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
 
-    before do
-        headers 'Access-Control-Allow-Origin' => '*',
-                'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
-                'Access-Control-Allow-Headers' => 'Content-Type'
-    end
+    # use Rack::Cors do
+    #     allow do
+    #       origins ''
+    #       resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+    #     end
+    # end
 
-    use Rack::Cors do
-        allow do
-          origins ''
-          resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-        end
-    end
+    # before do
+    #     headers 'Access-Control-Allow-Origin' => '*',
+    #             'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
+    #             'Access-Control-Allow-Headers' => 'Content-Type'
+    # end
 
     get '/jobs' do
         jobs = Job.all.order(:created_at)
@@ -23,6 +23,56 @@ class ApplicationController < Sinatra::Base
     get '/jobs/:id' do
         job = Job.find(params[:id]) 
         job.to_json(include: {applications: {include: :user}})
+    end
+
+    post '/jobs' do
+        begin
+            job = Job.create!(
+            title: params[:title],
+            description: params[:description],
+            company: params[:company],
+            location: params[:location],
+            salary: params[:salary]
+            )
+            { success: true, message: "Job created", job: job }.to_json
+          rescue ActiveRecord::RecordInvalid => e
+            status 422
+            { success: false, error: e.record.errors.full_messages }.to_json
+          rescue => e
+            status 500
+            { success: false, error: e.message }.to_json
+          end
+    end
+
+    patch '/jobs/:id' do
+        begin
+            job = Job.find(params[:id])
+            job.update!(params)
+          { success: true, message: "Job updated", job: job }.to_json
+        rescue ActiveRecord::RecordNotFound
+          status 404
+          { success: false, error: "Job not found" }.to_json
+        rescue ActiveRecord::RecordInvalid => e
+          status 422
+          { success: false, error: e.record.errors.full_messages }.to_json
+        rescue => e
+          status 500
+          { success: false, error: e.message }.to_json
+        end
+    end
+
+    delete '/jobs/:id' do
+        begin
+            job = Job.find(params[:id])
+            job.destroy
+            { success: true, message: "Job deleted" }.to_json
+          rescue ActiveRecord::RecordNotFound
+            status 404
+            { success: false, error: "Job not found" }.to_json
+          rescue => e
+            status 500
+            { success: false, error: e.message }.to_json
+          end
     end
 
     get '/users' do
@@ -42,6 +92,16 @@ class ApplicationController < Sinatra::Base
         applications.to_json
     end
 
+    get '/applications/:id' do
+        begin
+            application = Application.find(params[:id])
+            application.to_json
+        rescue ActiveRecord::RecordNotFound
+            status 404
+            { error: "Application not found" }.to_json
+        end
+    end
+
     get '/jobs/applications/:id' do
         job = Job.find(params[:id])
         applications = job.applications.to_json
@@ -53,19 +113,53 @@ class ApplicationController < Sinatra::Base
     end
 
     post '/applications' do
-        application = Application.create(
-         applicant_name: params[:applicant_name],
-         job_title: params[:job_title],
-         description: params[:description],
-         user_id: params[:user_id],
-         job_id: params[:job_id]) 
-        application.to_json
+        begin
+            application = Application.create!(
+              applicant_name: params[:applicant_name],
+              job_title: params[:job_title],
+              description: params[:description],
+              user_id: params[:user_id],
+              job_id: params[:job_id]
+            )
+            { success: true, message: "Application created", application: application }.to_json
+          rescue ActiveRecord::RecordInvalid => e
+            status 422
+            { success: false, error: e.record.errors.full_messages }.to_json
+          rescue => e
+            status 500
+            { success: false, error: e.message }.to_json
+          end
+    end
+
+    patch '/applications/:id' do
+        begin
+          application = Application.find(params[:id])
+          application.update!(params)
+          { success: true, message: "Application updated", application: application }.to_json
+        rescue ActiveRecord::RecordNotFound
+          status 404
+          { success: false, error: "Application not found" }.to_json
+        rescue ActiveRecord::RecordInvalid => e
+          status 422
+          { success: false, error: e.record.errors.full_messages }.to_json
+        rescue => e
+          status 500
+          { success: false, error: e.message }.to_json
+        end
     end
 
     delete '/applications/:id' do
-        application = Application.find(params[:id])
-        application.delete
-        "Deleted #{application.title}"
+        begin
+            application = Application.find(params[:id])
+            application.destroy
+            { success: true, message: "Application deleted" }.to_json
+          rescue ActiveRecord::RecordNotFound
+            status 404
+            { success: false, error: "Application not found" }.to_json
+          rescue => e
+            status 500
+            { success: false, error: e.message }.to_json
+          end
     end
 
 end
